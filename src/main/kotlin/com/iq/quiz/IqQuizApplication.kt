f6abd872f3bb2624505f36ca2f23033abc12c6cf
Angel2473 <angelea0424@gmail.com>
2025-04-28 12:33:16 +0530

    first commit

---
+++ src/main/kotlin/com/iq/quiz/IqQuizApplication.kt
@@ -0,0 +1,11 @@
+package com.iq.quiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IqQuizApplication
+
+fun main(args: Array<String>) {
+	runApplication<IqQuizApplication>(*args)
+}
---
+++ src/test/kotlin/com/iq/quiz/IqQuizApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iq.quiz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IqQuizApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
