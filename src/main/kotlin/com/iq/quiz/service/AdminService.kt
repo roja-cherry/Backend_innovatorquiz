c19df0e2ea17b40b37a6ae8a88696bb3cd9188e2
Angel2473 <angelea0424@gmail.com>
2025-05-06 12:20:58 +0530

    minor change

--- src/main/kotlin/com/iq/quiz/service/AdminService.kt
+++ src/main/kotlin/com/iq/quiz/service/AdminService.kt
@@ -116,7 +116,6 @@ class AdminService(
     }
 
     fun getQuizDto(quizId: String): QuizDTO? {
-//        val quiz= quizRepository.findByQuizId(quizId)?:throw ResponseStatusException(HttpStatus.NOT_FOUND,"Quiz Not Found")
         val quiz= quizRepository.findByQuizId(quizId)?:throw QuizNotFoundException("Quiz Not Found")
         return (quizToQuizDto(quiz))
     }
