902cdbf4a67d888cc58d2089581b9d06f71ae73b
Angel2473 <angelea0424@gmail.com>
2025-05-06 14:58:53 +0530

    added controller for filter

--- src/main/kotlin/com/iq/quiz/controller/AdminController.kt
+++ src/main/kotlin/com/iq/quiz/controller/AdminController.kt
@@ -2,11 +2,14 @@ package com.iq.quiz.controller
 
 import com.iq.quiz.Dto.QuizDTO
 import com.iq.quiz.Dto.QuizWithQuestionsDto
+import com.iq.quiz.Entity.QuizStatus
 import com.iq.quiz.service.AdminService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/admin/quiz")
@@ -53,6 +56,18 @@ class AdminController(
     fun deleteQuiz(@PathVariable id: String): ResponseEntity<String> {
         adminService.deleteQuizById(id)  // Calls the service method to delete the quiz
         return ResponseEntity.ok("Quiz deleted successfully")  // Returns a success message
+    }
+
+    @GetMapping("/quizzes")
+    fun getAllQuizzesForAdmin(
+        @RequestParam(required = false) search: String?,                     // search by name
+        @RequestParam(required = false) minDuration: Int?,                  // filter by min duration
+        @RequestParam(required = false) status: QuizStatus?,                // filter by status
+        @RequestParam(required = false)
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        createdAfter: LocalDateTime?                                        // filter by createdAt
+    ): List<QuizDTO> {
+        return adminService.getAllQuizzesForAdmin(search, minDuration, status, createdAfter)
     }
 
 
