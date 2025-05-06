da91834d477ee67be133dfc90042acc79af97181
roja-cherry <douglascherry98@gmail.com>
2025-05-06 14:50:45 +0530

    view & filter table quiz

--- src/main/kotlin/com/iq/quiz/controller/AdminController.kt
+++ src/main/kotlin/com/iq/quiz/controller/AdminController.kt
@@ -2,11 +2,14 @@ package com.iq.quiz.controller
 
 import com.iq.quiz.Dto.QuizDTO
 import com.iq.quiz.Dto.QuizWithQuestionsDto
+import com.iq.quiz.Entity.QuizStatus
 import com.iq.quiz.service.AdminService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/admin/quiz")
@@ -42,5 +45,9 @@ class AdminController(
         val response = adminService.getQuizDto(quizId)
         return ResponseEntity.status(HttpStatus.OK).body(response)
     }
+
+
+
+
 
 }
