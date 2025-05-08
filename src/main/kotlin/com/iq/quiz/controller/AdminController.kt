491769ec88e391255e5a6528568d701ef61ea7e3
Angel2473 <angelea0424@gmail.com>
2025-05-08 17:57:04 +0530

    controller changes

--- src/main/kotlin/com/iq/quiz/controller/AdminController.kt
+++ src/main/kotlin/com/iq/quiz/controller/AdminController.kt
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
+
 @RestController
 @RequestMapping("/api/admin/quiz")
 class AdminController(
