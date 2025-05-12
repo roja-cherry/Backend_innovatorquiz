94b35f2ac9d620329d0c23229a55e8d271d804d6
Jeena2001 <jjoy19847@gmail.com>
2025-05-12 17:16:58 +0530

    controller

--- src/main/kotlin/com/iq/quiz/controller/ScheduleController.kt
+++ src/main/kotlin/com/iq/quiz/controller/ScheduleController.kt
@@ -1,14 +1,12 @@
 package com.iq.quiz.controller
 
+import com.iq.quiz.Dto.ScheduleDto
 import com.iq.quiz.Dto.schedule.ScheduleEditCreateRequest
 import com.iq.quiz.Entity.Schedule
 import com.iq.quiz.service.ScheduleService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/schedule")
@@ -20,5 +18,15 @@ class ScheduleController(
     fun createNewSchedule(@RequestBody @Valid dto: ScheduleEditCreateRequest): ResponseEntity<Schedule> {
         val response = scheduleService.createNewQuiz(dto)
         return ResponseEntity.status(201).body(response)
+    }
+
+    @GetMapping("/{id}")
+    fun getScheduleById(@PathVariable id: String): ScheduleDto {
+        return scheduleService.getScheduleById(id)
+    }
+
+    @GetMapping("/All_Schedule")
+    fun getAllSchedules(): List<ScheduleDto> {
+        return scheduleService.getAllSchedules()
     }
 }
