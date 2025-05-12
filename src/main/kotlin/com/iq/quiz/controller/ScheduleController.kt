31f916405ebe1c5df9ffaeb68ec5187d28b2f5a5
Jeena2001 <jjoy19847@gmail.com>
2025-05-12 16:58:02 +0530

    Schedule get api controller and service added

--- src/main/kotlin/com/iq/quiz/controller/ScheduleController.kt
+++ src/main/kotlin/com/iq/quiz/controller/ScheduleController.kt
@@ -1,9 +1,26 @@
 package com.iq.quiz.controller
 
+import com.iq.quiz.Dto.QuizWithQuestionsDto
+import com.iq.quiz.Dto.ScheduleDto
+import com.iq.quiz.service.ScheduleService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/schedule")
-class ScheduleController {
+class ScheduleController (
+    private val scheduleService:ScheduleService
+){
+
+    @GetMapping("/{id}")
+    fun getScheduleById(@PathVariable id: String): ScheduleDto {
+        return scheduleService.getScheduleById(id)
+    }
+
+    @GetMapping("/All_Schedule")
+    fun getAllSchedules(): List<ScheduleDto> {
+        return scheduleService.getAllSchedules()
+    }
 }
--- src/main/kotlin/com/iq/quiz/service/ScheduleService.kt
+++ src/main/kotlin/com/iq/quiz/service/ScheduleService.kt
@@ -1,5 +1,5 @@
 package com.iq.quiz.service
-
+import com.iq.quiz.Dto.ScheduleDto
 import com.iq.quiz.Repository.QuizRepository
 import com.iq.quiz.Repository.ScheduleRepository
 import org.springframework.stereotype.Service
@@ -9,4 +9,35 @@ class ScheduleService(
     private val scheduleRepository: ScheduleRepository,
     private val quizRepository: QuizRepository
 ) {
+
+    fun getScheduleById(id: String): ScheduleDto {
+        val schedule = scheduleRepository.findById(id)
+            .orElseThrow { IllegalArgumentException("Schedule not found with ID: $id") }
+
+        return ScheduleDto(
+            id = schedule.id,
+            startDateTime = schedule.startDateTime,
+            endDateTime = schedule.endDateTime,
+            createdAt = schedule.createdAt,
+            updatedAt = schedule.updatedAt,
+            status = schedule.status
+        )
+    }
+
+    fun getAllSchedules(): List<ScheduleDto> {
+        return scheduleRepository.findAll().map { schedule ->
+            ScheduleDto(
+                id = schedule.id,
+                startDateTime = schedule.startDateTime,
+                endDateTime = schedule.endDateTime,
+                createdAt = schedule.createdAt,
+                updatedAt = schedule.updatedAt,
+                status = schedule.status
+            )
+        }
+    }
 }
+
+
+
+
