9317eaa5438b96e4fd41530d7e174a3d67d362f1
roja-cherry <douglascherry98@gmail.com>
2025-05-13 11:58:54 +0530

    commit for isscheduled

--- src/main/kotlin/com/iq/quiz/Repository/ScheduleRepository.kt
+++ src/main/kotlin/com/iq/quiz/Repository/ScheduleRepository.kt
@@ -2,11 +2,15 @@ package com.iq.quiz.Repository
 
 import com.iq.quiz.Entity.Quiz
 import com.iq.quiz.Entity.Schedule
+import com.iq.quiz.Entity.ScheduleStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ScheduleRepository: JpaRepository<Schedule, String> {
     fun findAllByQuiz(quiz: Quiz): List<Schedule>
+
+    // returns true if there’s any schedule in SCHEDULED (or LIVE) state for this quiz
+    fun existsByQuizQuizIdAndStatusIn(quizId: String, statuses: Collection<ScheduleStatus>): Boolean
 
 }
