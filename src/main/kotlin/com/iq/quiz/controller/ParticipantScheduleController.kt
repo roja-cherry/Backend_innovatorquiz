579ff585761f5e8cbcbcf77b5d2b505407191f07
SandhuBabu <sandhubabu704@gmail.com>
2025-05-20 10:44:56 +0530

    participant service added

--- src/main/kotlin/com/iq/quiz/controller/ParticipantScheduleController.kt
+++ src/main/kotlin/com/iq/quiz/controller/ParticipantScheduleController.kt
@@ -1,9 +1,12 @@
 package com.iq.quiz.controller
 
+import com.iq.quiz.service.ParticipantService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/participant")
-class ParticipantScheduleController {
+class ParticipantScheduleController(
+    private val participantService: ParticipantService
+) {
 }
